58b1b0265f82152a787a8f4b8fbb0aaef9787628
Ari Jordan <ari.jordan@gmx.ch>
2021-09-28 19:43:19 +0200

    Clean code using more Rust features

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #[allow(unused_imports)]
 use std::cmp::{min,max};
-use std::io::{BufWriter, stdin, stdout, Write};
+use std::io::stdin;
+// use std::io::{BufWriter, stdout, Write};
 use std::collections::VecDeque;
+use std::default::Default;
+use std::env;
+
+static USIZE_DEFAULT: usize = std::usize::MAX;
 
 #[derive(Default)]
 struct Scanner {
@@ -29,85 +34,105 @@ struct Edge{
     capacity : f64,
 }
 
+type Time = usize;
+type EdgeId = usize;
+type PacketId = usize;
+
 #[derive(Clone)]
 struct Packet{
     id : usize,
-    release_time : usize,
-    path : Vec<usize>,
-    entrance_time : usize,
-    path_position : usize,
-    edge_id : usize,
+    release_time : Time,
+    path : Vec<EdgeId>, // edges on path of packet
+    entrance_time : Option<Time>,
+    path_position : Option<usize>, // index in path : Vec<usize>
+}
+
+#[derive(Clone)]
+struct Vertex{
+    incoming_edges : Vec<EdgeId>,
+    outgoing_edges : Vec<EdgeId>,
 }
 
 struct Network{
-    n_vertices : usize,
-    edges : Vec::<Edge>,
-    edge_to_id : std::collections::HashMap::<(usize, usize), usize>,
-    packets : Vec::<Packet>,
-    queues : Vec<VecDeque<usize>>, // i-th queue corresponds to i-th edge
-    time : usize,
+    vertices : Vec<Vertex>,
+    edges : Vec<Edge>,
+    packets : Vec<Packet>,
+    queues : Vec<VecDeque<PacketId>>, // i-th queue corresponds to i-th edge
+    time : Time,
     packets_arrived : usize,
     arrival_times : Vec<usize>,
 }
 
+// struct State
+
 impl Network{
+
+    fn run_simulation(&mut self){
+        while self.packets_arrived < self.packets.len(){
+            self.timestep();
+        }
+    }
+
     fn timestep(&mut self){
         // TODO: split into two parts: movement and queueing
-        for packet_id in 0..self.packets.len(){
-            //let &mut packet = &mut self.packets[packet_id];
-            if self.packets[packet_id].release_time > self.time{
+        for (packet_id, packet) in self.packets.iter_mut().enumerate(){
+            if packet.release_time > self.time{
                 // Packet not ready yet
                 continue;
             }
-            else if self.packets[packet_id].release_time == self.time{
-                // Enter first edge on path
-                self.packets[packet_id].entrance_time = self.time;
-                self.packets[packet_id].path_position = self.packets[packet_id].path[0];
-                let p_edge_id = self.get_edge_id(self.packets[packet_id].path[0], self.packets[packet_id].path[1]);
-                self.packets[packet_id].edge_id = p_edge_id;               
-                self.queues[p_edge_id].push_back(packet_id);
-            }
-            else if self.time == self.packets[packet_id].entrance_time + self.edges[self.packets[packet_id].edge_id].length{
-                if self.packets[packet_id].path_position == self.packets[packet_id].path.len() - 1{
-                    // packet at end of path
-                    // TODO: save arrival time
-                    self.packets[packet_id].path_position = std::usize::MAX;
-                    self.packets[packet_id].edge_id = std::usize::MAX;
-                    self.packets_arrived += 1;
-                    self.arrival_times[packet_id] = self.time;
-                }
-                // packet changes into next edge
-                let p_edge_id = self.get_edge_id(self.packets[packet_id].path[self.packets[packet_id].path_position], self.packets[packet_id].path[self.packets[packet_id].path_position + 1]);
-                let popped_packet_id = self.queues[p_edge_id].pop_front().expect("pop_front() on empty queue called");
-                self.queues[p_edge_id].push_back(popped_packet_id); // TODO: correct order
-            }
             else{
-                // Do nothing
+                match packet.path_position{
+                    None =>{                        
+                        if packet.release_time == self.time{
+                            // Enter first edge on path, TODO: order compared to other entering packets
+                            let path_position = 0;  
+                            packet.path_position = Some(path_position);
+                            let edge_id = packet.path[path_position];
+                            packet.entrance_time = Some(self.time);                            
+                            self.queues[edge_id].push_back(packet_id);                    
+                        }
+                    }
+                    Some(path_position) =>{
+                        let edge_id = packet.path[path_position];
+                        match packet.entrance_time{
+                            None => println!("Error: packet.entrance_time is None"),
+                            Some(entrance_time) =>{
+                                if self.time == entrance_time +  self.edges[edge_id].length{
+                                    if path_position == packet.path.len() - 1{
+                                        // packet at end of path
+                                        packet.path_position = None;
+                                        self.packets_arrived += 1;
+                                        self.arrival_times[packet_id] = self.time;
+                                    }
+                                    else{
+                                        // packet changes into next edge                                                                      
+                                        let popped_packet_id = self.queues[edge_id].pop_front().expect("pop_front() on empty queue called");                                      
+                                        self.queues[packet.path[path_position + 1]].push_back(popped_packet_id); // TODO: correct order
+                                        packet.path_position = Some(path_position + 1); // TODO : check whether path_position is changed 
+                                    }
+                                }                           
+                            }
+                        }  
+                    }
+                }
             }
         }
         self.time += 1;
     }
-
-    // Return: id of edge (v_from, v_to)
-    fn get_edge_id(&mut self, v_from : usize, v_to : usize) -> usize{
-        // TODO: implement
-        *self.edge_to_id.get(&(v_from, v_to)).expect("edge doesn't exist")
-    }
 }
 
-fn main() {
+fn input() -> Network{
+    env::set_var("RUST_BACKTRACE", "full");
     let mut scan = Scanner::default();
-    let out = &mut BufWriter::new(stdout());
-
     let n_vertices : usize = scan.next::<usize>();
     let n_edges : usize = scan.next::<usize>();
     let mut edges : Vec::<Edge> = Vec::<Edge>::new();
-    let mut edge_to_id = std::collections::HashMap::<(usize, usize), usize>::new();
+    let mut vertices : Vec<Vertex> = vec![Vertex{incoming_edges : Vec::<usize>::new(), outgoing_edges : Vec::<usize>::new()}; n_vertices];
     for edge_id in 0..n_edges{
         let v_from = scan.next::<usize>();
-        assert!(v_from < n_vertices, "vertex indices should be in [0, n_edges)");
+        assert!(v_from < n_vertices, "vertex indices should be in [0, n_vertices)");
         let v_to = scan.next::<usize>();
-        assert!(v_to < n_vertices, "vertex indices should be in [0, n_edges)");
+        assert!(v_to < n_vertices, "vertex indices should be in [0, n_vertices)");
         let length = scan.next::<usize>();
         assert!(length > 0, "edge lengths should be positive");
         let capacity = scan.next::<f64>();
@@ -121,7 +146,8 @@ fn main() {
                 capacity : capacity,           
             }
         );
-        edge_to_id.insert((v_from, v_to), edge_id);
+        vertices[v_from].outgoing_edges.push(v_to);
+        vertices[v_to].incoming_edges.push(v_from);
     }
 
     let n_packets : usize = scan.next::<usize>();
@@ -136,30 +162,49 @@ fn main() {
                 id : packet_id,
                 release_time : release_time,
                 path : path,
-                entrance_time : std::usize::MAX, // default value
-                path_position : std::usize::MAX, // default value
-                edge_id : std::usize::MAX, // default value
+                entrance_time : None,
+                path_position : None,
             }
         );
     }
-    let mut network = Network{
-        n_vertices : n_vertices,
-        edges : edges,
-        edge_to_id : edge_to_id,
+    let network = Network{
+        vertices : vertices,
+        edges : edges,    
         packets : packets,
         queues : vec![VecDeque::new(); n_edges],
         time : 0,
         packets_arrived : 0,
-        arrival_times : vec![n_packets; 0],
+        arrival_times : vec![USIZE_DEFAULT, n_packets],
     };
+    network
+}
 
-    while network.packets_arrived < network.packets.len(){
-        network.timestep();
+fn output_arrivals(network : &Network){
+    //let out = &mut BufWriter::new(stdout());
+    println!("Last arrival time:\n{}\n\nIndividual arrival times:", network.arrival_times.iter().max().expect("max failed somehow"));
+    for arrival in &network.arrival_times{
+        print!("{} ", arrival);
     }
+}
 
-    write!(out, "Last arrival time:\n{}\n\nIndividual arrival times:", network.arrival_times.iter().max().expect("max failed somehow"));
+fn main() {    
+    let mut network = input();
+    network.run_simulation();
+    output_arrivals(&network);
+}
 
-    for arrival in network.arrival_times{
-        write!(out, "{} ", arrival);
-    }
+#[test]
+fn test_testing(){
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+fn test_empty(){
+    let mut network = input();
+    network.run_simulation();
+    assert_eq!(network.packets.len(), 0);
+    assert_eq!(network.packets_arrived, 0);
+    assert_eq!(network.arrival_times.len(), 0);
+    assert_eq!(network.queues.len(), network.edges.len());
+    assert_eq!(network.time, 0);
 }
