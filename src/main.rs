874647e8b3c607362743fdae10ed722705adee0b
Ari Jordan <ari.jordan@gmx.ch>
2021-09-29 13:12:35 +0200

    Add simple instances and tests

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use std::io::stdin;
 // use std::io::{BufWriter, stdout, Write};
 use std::collections::VecDeque;
 use std::default::Default;
-use std::env;
 
-static USIZE_DEFAULT: usize = std::usize::MAX;
+const USIZE_DEFAULT: usize = std::usize::MAX;
 
 #[derive(Default)]
 struct Scanner {
@@ -25,22 +24,25 @@ impl Scanner {
     }
 }
 
-#[derive(Clone)]
-struct Edge{
-    id : usize,
-    v_from : usize,
-    v_to : usize,
-    length : usize,
-    capacity : f64,
-}
-
 type Time = usize;
+type VertexId = usize;
 type EdgeId = usize;
 type PacketId = usize;
 
 #[derive(Clone)]
+struct Edge{
+    id : EdgeId,
+    v_from : VertexId,
+    v_to : VertexId,
+    length : usize,
+    capacity : f64,
+}
+
+
+
+#[derive(Clone)]
 struct Packet{
-    id : usize,
+    id : PacketId,
     release_time : Time,
     path : Vec<EdgeId>, // edges on path of packet
     entrance_time : Option<Time>,
@@ -60,10 +62,10 @@ struct Network{
     queues : Vec<VecDeque<PacketId>>, // i-th queue corresponds to i-th edge
     time : Time,
     packets_arrived : usize,
-    arrival_times : Vec<usize>,
+    arrival_times : Vec<Time>,
 }
 
-// struct State
+// TODO: separaze Network into structs Network + State
 
 impl Network{
 
@@ -84,7 +86,7 @@ impl Network{
                 match packet.path_position{
                     None =>{                        
                         if packet.release_time == self.time{
-                            // Enter first edge on path, TODO: order compared to other entering packets
+                            // Enter first edge on path, TODO: order compared to other packets entering edge
                             let path_position = 0;  
                             packet.path_position = Some(path_position);
                             let edge_id = packet.path[path_position];
@@ -117,32 +119,36 @@ impl Network{
                 }
             }
         }
-        self.time += 1;
+        if self.packets_arrived < self.packets.len(){
+            self.time += 1;
+        }
     }
 }
 
-fn vertex_path_to_edge_path(vertex_path : Vec<usize>, edge_to_id : &std::collections::HashMap::<(usize, usize), usize>) -> Vec<EdgeId>{
+fn vertex_path_to_edge_path(vertex_path : Vec<VertexId>, edge_to_id : &std::collections::HashMap::<(VertexId, VertexId), EdgeId>) -> Vec<EdgeId>{
     assert!(vertex_path.len() > 0);
     let mut edge_path = Vec::<EdgeId>::new();
-    for id in 0..vertex_path.len(){
+    for id in 0..vertex_path.len()-1{
         edge_path.push(edge_to_id[&(vertex_path[id], vertex_path[id + 1])]);
     }
     assert_eq!(edge_path.len(), vertex_path.len() - 1);
     edge_path
 }
 
-fn input() -> Network{
-    env::set_var("RUST_BACKTRACE", "full");
-    let mut scan = Scanner::default();
+fn input(filename : &str) -> Network{
+    //env::set_var("RUST_BACKTRACE", "full");
+    let input_string = std::fs::read_to_string(filename).expect("Error while reading file");
+    let buffer : Vec<String> = input_string.split_whitespace().rev().map(String::from).collect();
+    let mut scan = Scanner{buffer : buffer};
     let n_vertices : usize = scan.next::<usize>();
     let n_edges : usize = scan.next::<usize>();
     let mut edges : Vec::<Edge> = Vec::<Edge>::new();
-    let mut vertices : Vec<Vertex> = vec![Vertex{incoming_edges : Vec::<usize>::new(), outgoing_edges : Vec::<usize>::new()}; n_vertices];
-    let mut edge_to_id = std::collections::HashMap::<(usize, usize), EdgeId>::new();
+    let mut vertices : Vec<Vertex> = vec![Vertex{incoming_edges : Vec::<VertexId>::new(), outgoing_edges : Vec::<VertexId>::new()}; n_vertices];
+    let mut edge_to_id = std::collections::HashMap::<(VertexId, VertexId), EdgeId>::new();
     for edge_id in 0..n_edges{
-        let v_from = scan.next::<usize>();
+        let v_from = scan.next::<VertexId>();
         assert!(v_from < n_vertices, "vertex indices should be in [0, n_vertices)");
-        let v_to = scan.next::<usize>();
+        let v_to = scan.next::<VertexId>();
         assert!(v_to < n_vertices, "vertex indices should be in [0, n_vertices)");
         let length = scan.next::<usize>();
         assert!(length > 0, "edge lengths should be positive");
@@ -163,11 +169,12 @@ fn input() -> Network{
     }
 
     let n_packets : usize = scan.next::<usize>();
+    println!("n_packets: {}", n_packets);
     let mut packets = Vec::<Packet>::new();
     for packet_id in 0..n_packets{
-        let release_time = scan.next::<usize>();
+        let release_time = scan.next::<Time>();
         let path_length = scan.next::<usize>();
-        let vertex_path : Vec<usize> = (0..path_length).map(|_| scan.next::<usize>()).collect();
+        let vertex_path : Vec<VertexId> = (0..path_length).map(|_| scan.next::<VertexId>()).collect();
         assert!(vertex_path.len() >= 2, "paths should have length at least 2");
         packets.push(
             Packet{
@@ -186,7 +193,7 @@ fn input() -> Network{
         queues : vec![VecDeque::new(); n_edges],
         time : 0,
         packets_arrived : 0,
-        arrival_times : vec![USIZE_DEFAULT, n_packets],
+        arrival_times : vec![USIZE_DEFAULT; n_packets],
     };
     network
 }
@@ -199,24 +206,43 @@ fn output_arrivals(network : &Network){
     }
 }
 
-fn main() {    
-    let mut network = input();
+fn main() -> std::io::Result<()>{
+    let mut network = input("src/instances/instance_l.txt");
     network.run_simulation();
     output_arrivals(&network);
+    Ok(())
 }
 
 #[test]
 fn test_testing(){
+    assert!(true);
     assert_eq!(2 + 2, 4);
+    assert_ne!(2 + 2, 5);
 }
 
 #[test]
 fn test_empty(){
-    let mut network = input();
+    let mut network = input("src/instances/instance_empty.txt");
     network.run_simulation();
+    assert_eq!(network.vertices.len(), 0);
+    assert_eq!(network.edges.len(), 0);
     assert_eq!(network.packets.len(), 0);
     assert_eq!(network.packets_arrived, 0);
     assert_eq!(network.arrival_times.len(), 0);
     assert_eq!(network.queues.len(), network.edges.len());
     assert_eq!(network.time, 0);
+}
+
+#[test]
+fn test_i(){
+    let mut network = input("src/instances/instance_i.txt");
+    network.run_simulation();
+    assert_eq!(network.vertices.len(), 2);
+    assert_eq!(network.edges.len(), 1);
+    assert_eq!(network.packets.len(), 1);
+    assert_eq!(network.packets_arrived, 1);
+    assert_eq!(network.arrival_times.len(), 1);
+    assert_eq!(network.arrival_times[0], 1);
+    assert_eq!(network.queues.len(), 1);
+    assert_eq!(network.time, 1);
 }
