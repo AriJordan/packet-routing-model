53bd808b1b3dd6cf82a2e6538602891a0f236502
Ari Jordan <ari.jordan@gmx.ch>
2021-11-22 17:22:37 +0100

    small script change description

--- runApproxError1D.py
+++ runApproxError1D.py
@@ -3,16 +3,15 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     SHOW_PLOT = True
-    SAVE_PLOT = False
-    DESCRIPTION = "Error with b = a**2"
+    SAVE_PLOT = False  
     parser = ArgumentParser(description="Compare Packet Routing to Multi-Commodity Flows Over Time")
     parser.add_argument("instance_name", type=str, help="name of instance to run")
     args = parser.parse_args()
     INSTANCE_NAME = args.instance_name
-
-    N = 3
-    ALPHAS = [0.5**i for i in range(N)]
+    N = 8
+    ALPHAS = [0.8**i for i in range(N)]
     # b = a**2
+    DESCRIPTION = "Error with b = a^2"
     BETAS = [ALPHAS[i]**2 for i in range(N)]
     callSimulation.multiple_runs(INSTANCE_NAME=INSTANCE_NAME, alphas=ALPHAS, betas=BETAS,
         show_plot=SHOW_PLOT, save_plot=SAVE_PLOT, description=DESCRIPTION)
