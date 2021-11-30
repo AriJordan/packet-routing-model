cad3b8a6a67fa1621267520c18b2e641a7a085f6
Ari Jordan <ari.jordan@gmx.ch>
2021-11-30 10:26:05 +0100

    Generate "Packets Vs Flow" plots

--- runSingleComparison.py
+++ runSingleComparison.py
@@ -2,6 +2,6 @@ from python_impl import callSimulation, parseArgs
 
 if __name__ == "__main__":
     SHOW_PLOT = True
-    SAVE_PLOT = False
+    SAVE_PLOT = True
     INSTANCE_NAME, ALPHA, BETA = parseArgs.parse_instance_name_alpha_beta()
     callSimulation.single_run(INSTANCE_NAME=INSTANCE_NAME, alpha=ALPHA, beta=BETA, show_plot=SHOW_PLOT, save_plot=SAVE_PLOT)
