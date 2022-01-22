c39cf26a8520e97cfe894fb9b7957b4ea94852b5
Ari Jordan <ari.jordan@gmx.ch>
2022-01-22 11:23:53 +0100

    Add instance short_i

--- runApproxError1D.py
+++ runApproxError1D.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     INSTANCE_NAME = parseArgs.parse_instance_name()
     SHOW_PLOT = True
     SAVE_PLOT = False  
-    N = 6
+    N = 10
     DESCRIPTION = "Error with beta = (alpha)^2"
-    ALPHAS = [0.5**i for i in range(N)]
-    BETAS = [ALPHAS[i]**2 for i in range(N)] # beta = alpha^2
+    ALPHAS = [(i+1)/N for i in range(N)]
+    BETAS = [ALPHAS[i]**2 for i in range(N)]
     callSimulation.multiple_runs(INSTANCE_NAME=INSTANCE_NAME, alphas=ALPHAS, betas=BETAS,
         show_plot=SHOW_PLOT, save_plot=SAVE_PLOT, description=DESCRIPTION)
